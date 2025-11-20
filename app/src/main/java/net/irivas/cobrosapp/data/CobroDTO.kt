651efbece3872f985cd5f3e9aec2cc2feaca08cf
KevinU20221275 <U20221275@univo.edu.sv>
2025-11-20 16:42:28 -0600

    se modifico el CobroDTO.kt

--- app/src/main/java/net/irivas/cobrosapp/data/CobroDTO.kt
+++ app/src/main/java/net/irivas/cobrosapp/data/CobroDTO.kt
@@ -1,3 +1,11 @@
 package net.irivas.cobrosapp.data
 
-data class CobroDTO()
+data class CobroDTO(
+    val idCobro: Int,
+    val nombreComerciante: String?,
+    val numeroPuesto: String?,
+    val monto: Double,
+    val recibido: Double,
+    val vuelto: Double,
+    val fecha: String
+)
