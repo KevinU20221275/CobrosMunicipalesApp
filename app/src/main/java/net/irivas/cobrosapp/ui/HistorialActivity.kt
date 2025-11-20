b1e0985dd8975082881185de83b61b830d78118b
KevinU20221275 <U20221275@univo.edu.sv>
2025-11-20 16:44:07 -0600

    se modifico el HistorialActivity.kt

--- app/src/main/java/net/irivas/cobrosapp/ui/HistorialActivity.kt
+++ app/src/main/java/net/irivas/cobrosapp/ui/HistorialActivity.kt
@@ -1,34 +1,32 @@
 package net.irivas.cobrosapp.ui
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import net.irivas.cobrosapp.R
-import net.irivas.cobrosapp.adapters.PagosAdapter
-import net.irivas.cobrosapp.data.Pago
-import net.irivas.cobrosapp.data.PagosDBHelper
+import net.irivas.cobrosapp.adapters.CobrosAdapter
+import net.irivas.cobrosapp.data.CobroDTO
+import net.irivas.cobrosapp.data.CobrosDBHelper
 
 class HistorialActivity : AppCompatActivity() {
     private lateinit var recyclerPagos: RecyclerView
-    private lateinit var adapter: PagosAdapter
+    private lateinit var adapter: CobrosAdapter
     private lateinit var inputSearch: EditText
     private lateinit var txtTotal: TextView
-    private lateinit var db: PagosDBHelper
+    private lateinit var db: CobrosDBHelper
 
 
-    private val listaOriginal = mutableListOf<Pago>()
-    private val listaFiltrada = mutableListOf<Pago>()
+    private val listaOriginal = mutableListOf<CobroDTO>()
+    private val listaFiltrada = mutableListOf<CobroDTO>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_historial)
 
-        db = PagosDBHelper(this)
+        db = CobrosDBHelper(this)
         recyclerPagos = findViewById(R.id.recyclerPagos)
         inputSearch = findViewById(R.id.inputSearch)
         txtTotal = findViewById(R.id.txtTotalValue)
@@ -38,21 +36,21 @@ class HistorialActivity : AppCompatActivity() {
         listaFiltrada.clear()
 
         // carga los pagos desde la db
-        listaOriginal.addAll(db.obtenerPagos())
+        listaOriginal.addAll(db.obtenerCobrosConInfo())
 
         // copia la lista original a la lista que usaremos para filtrar
         listaFiltrada.addAll(listaOriginal)
 
-        adapter = PagosAdapter(listaFiltrada)
+        adapter = CobrosAdapter(listaFiltrada, db)
         recyclerPagos.layoutManager = LinearLayoutManager(this)
         recyclerPagos.adapter = adapter
 
         calcularTotal()
 
-        configurarBusqueda()
+        //configurarBusqueda()
     }
 
-    private fun configurarBusqueda() {
+    /* private fun configurarBusqueda() {
         inputSearch.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {}
 
@@ -73,7 +71,7 @@ class HistorialActivity : AppCompatActivity() {
                 calcularTotal()
             }
         })
-    }
+    }*/
 
     private fun calcularTotal() {
         val total = listaFiltrada.sumOf { it.monto }
