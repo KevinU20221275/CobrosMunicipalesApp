ea0b25b95c610431de2c6907bd495959a3b02c2b
KevinU20221275 <U20221275@univo.edu.sv>
2025-11-20 16:50:02 -0600

    Se agregaron enlaces temporales a las nuevas funcionalidades para testing

--- app/src/main/java/net/irivas/cobrosapp/MainActivity.kt
+++ app/src/main/java/net/irivas/cobrosapp/MainActivity.kt
@@ -5,12 +5,22 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import net.irivas.cobrosapp.ui.HistorialActivity
+import net.irivas.cobrosapp.ui.ListarComerciantesActivity
 import net.irivas.cobrosapp.ui.RegistrarCobroActivity
+import net.irivas.cobrosapp.ui.RegistrarPuestoActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnListarComerciantes).setOnClickListener {
+            startActivity(Intent(this, ListarComerciantesActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.btnRegistrarPuesto).setOnClickListener {
+            startActivity(Intent(this, RegistrarPuestoActivity::class.java))
+        }
 
         findViewById<Button>(R.id.btnRegistrarCobro).setOnClickListener {
             startActivity(Intent(this, RegistrarCobroActivity::class.java))
